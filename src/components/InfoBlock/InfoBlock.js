c5223a80d9b0fac9a3483cac42e5190fe8ee7fb9
Don Hsieh <don.shieh@gmail.com>
2017-07-27 23:03:45 +0800

    💄 Add animation for first enter

--- src/components/InfoBlock/InfoBlock.js
+++ src/components/InfoBlock/InfoBlock.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './InfoBlock.css'
 
-const InfoBlock = (props) => {
-  return (
-    <div className='Info-Block'>
-      <div className='bg-block' />
-      <div className='content-block'>
-        <div className='title'>{props.title}</div>
-        <div className='list-container'>
-          {props.children}
-          {/* <ul>
-            <li>CSS3 (flex-box and those <em>fancy</em> stuff about text, background and filter)</li>
-            <li>Sass</li>
-            <li>PostCSS</li>
-            <li>susy & breakpoint (for RWD)</li>
-          </ul> */}
+class InfoBlock extends Component {
+  beforeEnterClassName = 'before-enter'
+
+  componentDidMount () {
+    if (IntersectionObserver) {
+      const io = new IntersectionObserver(
+        entries => {
+          entries.map(entry => {
+            console.log(entry.target)
+            entry.target.classList.remove(this.beforeEnterClassName)
+          })
+        }
+      )
+
+      io.observe(this.ele)
+    }
+  }
+
+  render () {
+    const { id, title, children } = this.props
+
+    return (
+      <div
+        className={`Info-Block ${this.beforeEnterClassName}`}
+        ref={ele => { this.ele = ele }}
+        id={id}
+      >
+        <div className='bg-block' />
+        <div className='content-block'>
+          <div className='title'>{title}</div>
+          <div className='list-container'>
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default InfoBlock
